1839c287c8b44be3dd18071132ebc73b6451b8dc
Michel <coolnerdcool@gmail.com>
2017-12-19 20:31:14 -0600

    Fix(CameraViewController): Syntax Error

--- VisualRecognition/CameraViewController.swift
+++ VisualRecognition/CameraViewController.swift
@@ -72,6 +72,26 @@ class CameraViewController: UIViewController, AVCapturePhotoCaptureDelegate {
     
     func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
         
+        if let error = error{
+            print("\ERROR: \(error)")
+            return
+        }
+        
+        let photoData = photo.fileDataRepresentation()
+        let dataProvider = CGDataProvider(data: photoData! as CFData)
+        
+        let CGImageRef = CGImage(jpegDataProviderSource: dataProvider!,
+                                 decode: nil,
+                                 shouldInterpolate: true,
+                                 intent: .defaultIntent)
+        
+        classify(CGImageRef!) { data in
+            self.push(data: data)
+        }
+        let image = UIImage(data: photoData!)
+        self.tempImageView.image = image
+        self.tempImageView.isHidden = false
+        
     }
     
     func classify(_ image: CGImage, completion: @escaping ([VNClassificationObservation])-> Void) {
